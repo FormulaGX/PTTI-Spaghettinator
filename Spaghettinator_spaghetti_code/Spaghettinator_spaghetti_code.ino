90d9437d59460c1dfcc0d164d13c6bdb392f906d
FormulaGX <dmixhs@gmail.com>
2024-04-26 13:42:38 -0400

    Added some comments and commented out unused code.

--- Spaghettinator_spaghetti_code/Spaghettinator_spaghetti_code.ino
+++ Spaghettinator_spaghetti_code/Spaghettinator_spaghetti_code.ino
@@ -1,5 +1,15 @@
+/*
+  This sketch was written by Anthony Chappell a PTTI project using an Arduino Uno board.
+  This sketch was originally designed to work with a pair of proximity sensors mounted besides a conveyor belt;
+  it was later modified to work with the the ultrasonic distance sensor.
+
+  The Arduino is connected to a 16/2 LCD display with an I2C backpack extension, an ultrasonic sensor (sonar), and a motor stepper.
+  The sonar is pinged every 250ms and the distance is displayed on the LCD every ping cycle.
+  The the arm on the motor extends when the object on the conveyor belt is 12cm away from the sonar,
+  then it will retract after 1 second. The motor cannot move any sooner than 1 second after it retracts.
+*/
+
 #include <AccelStepper.h>
-#include <MultiStepper.h>
 #include <Wire.h>
 #include <hd44780.h>
 #include <hd44780ioClass/hd44780_I2Cexp.h>
@@ -15,19 +25,19 @@
 #define SONAR_MAX_DISTANCE 400
 
 // Proximity Sensor Setup Data
-#define PROX_PIN 4
-bool proxStatus;
+// #define PROX_PIN 4 // Proximity sensor input pin
+// bool proxStatus; // Proximity sensor detection status
 
 // Motor Setup Data
-#define dirPin 2
-#define stepPin 3
-#define motorInterfaceType 1
+#define DIR_PIN 2
+#define STEP_PIN 3
+#define MOTOR_INTERFACE_TYPE 1
 int motorState; // 0 = stationary/retracted, 1 = stationary/extended
 
 // Devices
 hd44780_I2Cexp lcd; //LCD display with I2C backpack extension
 NewPing sonar(SONAR_TRIGGER_PIN, SONAR_ECHO_PIN, SONAR_MAX_DISTANCE); //Sonar sensor
-AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin); // Stepper motor
+AccelStepper stepper = AccelStepper(MOTOR_INTERFACE_TYPE, STEP_PIN, DIR_PIN); // Stepper motor
 
 // Time variables
 unsigned long currentMillis, sonarMillis, motorMillis;
@@ -40,13 +50,13 @@ float duration, distance;
 void setup() {
 
   Serial.begin(9600); // Establish connection with the device at 9.6Kb/s
-  pinMode(PROX_PIN, INPUT_PULLUP); // Use the proximity sensor pin
+  // pinMode(PROX_PIN, INPUT_PULLUP); // Use the proximity sensor pin
 
   int lcd_status; // LCD error code variable
 
   // Set the timers for the sonar and motors in milliseconds
-  sonarMillis = 0;
-  motorMillis = 0;
+  sonarMillis = 0; //Ultrasonic sensor timer
+  motorMillis = 0; //Stepper motor timer
 
   // Set motor status
   motorState = 0;
@@ -79,7 +89,7 @@ void setup() {
 void loop() {
 
   currentMillis = millis(); // Retrieve the program's uptime in milliseconds
-  proxStatus = digitalRead(PROX_PIN); // Read the status of the proximity sensor pin
+  // proxStatus = digitalRead(PROX_PIN); // Read the status of the proximity sensor pin
 
   // Sonar controls
   // If 250 milliseconds have elapsed, or the program has just started, do the following:
@@ -107,10 +117,11 @@ void loop() {
 
   // Motor controls
   // This switch statement checks the motor state integer
-  // 0 = stationary/retracted, 1 = stationary/extended, 2 = moving/retracting, 4 = moving/extending
+  // 0 = stationary/retracted, 1 = stationary/extended
 
   if (motorState == 0) {
-    if ((currentMillis - motorMillis >= 1000 || motorMillis == 0) && distance <= 7) {
+
+    if ((currentMillis - motorMillis >= 1000 || motorMillis == 0) && distance <= 15) {
       
       motorState = 1; // Extend the motor
       motorMillis = currentMillis;
@@ -118,12 +129,14 @@ void loop() {
       stepper.moveTo(80); // Set the target position
       stepper.runToPosition();
     }
+
     else if (currentMillis - motorMillis >= 250) {
       
       lcd.setCursor(0,1);
       lcd.print("Waiting");
     }
   }
+
   if (motorState == 1) {
     
         if (currentMillis - motorMillis >= 1000) {
